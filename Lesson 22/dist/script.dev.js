ebd719d0659ced6a99a24ceee444b42e26762626
KaterynaStadnichenko <iloveua.work@gmail>
2024-08-11 16:57:15 +0300

    js2

---
+++ Lesson 22/dist/script.dev.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var userNumber = prompt("pls enter three-digit number");
+var correctNum = 123;
+alert();
+var today = new Date();
+var tomorrow = new Date();
+today.getDate();
+tomorrow.getDate();
+tomorrow.setDate(today.getDate() + 1);
+tomorrow.toDateString();
---
+++ Lesson 22/script.js
@@ -0,0 +1,11 @@
+let userNumber = prompt("pls enter three-digit number");
+const correctNum = 123;
+alert()
+
+
+const today = new Date();
+const tomorrow = new Date();
+today.getDate()
+tomorrow.getDate()
+tomorrow.setDate(today.getDate() + 1);
+tomorrow.toDateString();
