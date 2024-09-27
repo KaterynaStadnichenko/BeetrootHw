b126406eaeeb55647936e47b426f6370d90118b7
KaterynaStadnichenko <iloveua.work@gmail>
2024-09-27 18:46:42 +0300

    lesson 33

--- Lesson 33/dist/script.dev.js
+++ Lesson 33/dist/script.dev.js
@@ -1,43 +1,53 @@
 "use strict";
 
 // const dataElement = document.querySelector('.playList');
-// let playList = [
-//     {
-//      author: "LED ZEPPELIN",
-//      song:"STAIRWAY TO HEAVEN"
-//     },
-//     {
-//      author: "QUEEN",
-//      song:"BOHEMIAN RHAPSODY"
-//     },
-//     {
-//      author: "LYNYRD SKYNYRD",
-//      song:"FREE BIRD"
-//     },
-//     {
-//      author: "DEEP PURPLE",
-//      song:"SMOKE ON THE WATER"
-//     },
-//     {
-//      author: "JIMI HENDRIX",
-//      song:"ALL ALONG THE WATCHTOWER"
-//     },
-//     {
-//      author: "AC/DC",
-//      song:"BACK IN BLACK"
-//     },
-//     {
-//      author: "QUEEN",
-//      song:"WE WILL ROCK YOU"
-//     },
-//     {
-//      author: "METALLICA",
-//      song:"ENTER SANDMAN"
-//     }
-//     ];
+var playList = [{
+  author: "LED ZEPPELIN",
+  song: "STAIRWAY TO HEAVEN"
+}, {
+  author: "QUEEN",
+  song: "BOHEMIAN RHAPSODY"
+}, {
+  author: "LYNYRD SKYNYRD",
+  song: "FREE BIRD"
+}, {
+  author: "DEEP PURPLE",
+  song: "SMOKE ON THE WATER"
+}, {
+  author: "JIMI HENDRIX",
+  song: "ALL ALONG THE WATCHTOWER"
+}, {
+  author: "AC/DC",
+  song: "BACK IN BLACK"
+}, {
+  author: "QUEEN",
+  song: "WE WILL ROCK YOU"
+}, {
+  author: "METALLICA",
+  song: "ENTER SANDMAN"
+}];
+var playListDiv = document.getElementById("song-list");
+playList.forEach(function (playListItem) {
+  var songsDiv = document.createElement("div");
+  var authorName = document.createElement("p");
+  authorName.textContent = playListItem.author;
+  var songName = document.createElement("p");
+  songName.textContent = playListItem.song;
+  songsDiv.appendChild(authorName);
+  songsDiv.appendChild(songName);
+  playListDiv.appendChild(songsDiv);
+});
+
 function showList() {
-  var modal = document.querySelector(".modal");
+  var modal = document.querySelector(".song-list");
   var button = document.querySelector(".show_button");
-  modal.classList.contains("visible") ? button.textContent = 'open' : button.textContent = "close";
+
+  if (modal.classList.contains("visible")) {
+    button.textContent = "open";
+  } else {
+    button.textContent = "close";
+  }
+
   modal.classList.toggle("visible");
-}
+} // modal.classList.contains("visible") ? button.textContent = 'open' : button.textContent = "close"
+// modal.classList.toggle("visible")
--- Lesson 33/script.js
+++ Lesson 33/script.js
@@ -1,48 +1,67 @@
 // const dataElement = document.querySelector('.playList');
 
-// let playList = [
-//     {
-//      author: "LED ZEPPELIN",
-//      song:"STAIRWAY TO HEAVEN"
-//     },
-//     {
-//      author: "QUEEN",
-//      song:"BOHEMIAN RHAPSODY"
-//     },
-//     {
-//      author: "LYNYRD SKYNYRD",
-//      song:"FREE BIRD"
-//     },
-//     {
-//      author: "DEEP PURPLE",
-//      song:"SMOKE ON THE WATER"
-//     },
-//     {
-//      author: "JIMI HENDRIX",
-//      song:"ALL ALONG THE WATCHTOWER"
-//     },
-//     {
-//      author: "AC/DC",
-//      song:"BACK IN BLACK"
-//     },
-//     {
-//      author: "QUEEN",
-//      song:"WE WILL ROCK YOU"
-//     },
-//     {
-//      author: "METALLICA",
-//      song:"ENTER SANDMAN"
-//     }
-//     ];
+let playList = [
+  {
+    author: "LED ZEPPELIN",
+    song: "STAIRWAY TO HEAVEN",
+  },
+  {
+    author: "QUEEN",
+    song: "BOHEMIAN RHAPSODY",
+  },
+  {
+    author: "LYNYRD SKYNYRD",
+    song: "FREE BIRD",
+  },
+  {
+    author: "DEEP PURPLE",
+    song: "SMOKE ON THE WATER",
+  },
+  {
+    author: "JIMI HENDRIX",
+    song: "ALL ALONG THE WATCHTOWER",
+  },
+  {
+    author: "AC/DC",
+    song: "BACK IN BLACK",
+  },
+  {
+    author: "QUEEN",
+    song: "WE WILL ROCK YOU",
+  },
+  {
+    author: "METALLICA",
+    song: "ENTER SANDMAN",
+  },
+];
 
-   
+const playListDiv = document.getElementById("song-list");
 
- 
-    function showList() {
-        const modal = document.querySelector(".modal")
-        const button = document.querySelector(".show_button")
+playList.forEach((playListItem) => {
+  const songsDiv = document.createElement("div");
 
-        modal.classList.contains("visible") ? button.textContent = 'open' : button.textContent = "close"
-        modal.classList.toggle("visible")
-    }
+  const authorName = document.createElement("p");
+  authorName.textContent = playListItem.author;
 
+  const songName = document.createElement("p");
+  songName.textContent = playListItem.song;
+
+  songsDiv.appendChild(authorName);
+  songsDiv.appendChild(songName);
+  playListDiv.appendChild(songsDiv);
+});
+
+function showList() {
+  const modal = document.querySelector(".song-list");
+  const button = document.querySelector(".show_button");
+
+  if (modal.classList.contains("visible")) {
+    button.textContent = "open";
+  } else {
+    button.textContent = "close";
+  }
+  modal.classList.toggle("visible");
+}
+
+// modal.classList.contains("visible") ? button.textContent = 'open' : button.textContent = "close"
+// modal.classList.toggle("visible")
