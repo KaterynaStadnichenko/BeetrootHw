e1d4b81534e0ed42d80caff5151a0529a14b8679
KaterynaStadnichenko <iloveua.work@gmail>
2024-10-11 17:12:26 +0300

    slider problem

---
+++ Project 2/dist/script.dev.js
@@ -0,0 +1,9 @@
+"use strict";
+
+$(document).ready(function () {
+  $('.news_slider_container').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3
+  });
+});
---
+++ Project 2/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+$('.news_slider_container').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3
+  });
+});
+          
