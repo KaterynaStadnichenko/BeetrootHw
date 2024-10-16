3997dee6901a178e59fbdc475340e4f93d81dd52
KaterynaStadnichenko <iloveua.work@gmail>
2024-10-16 19:15:05 +0300

    upd

--- Project 2/dist/script.dev.js
+++ Project 2/dist/script.dev.js
@@ -1,24 +1,5 @@
 "use strict";
 
-var _lightgallery = _interopRequireDefault(require("https://cdn.skypack.dev/lightgallery@2.1.2"));
-
-var _zoom = _interopRequireDefault(require("https://cdn.skypack.dev/lightgallery@2.1.2/plugins/zoom"));
-
-var _thumbnail = _interopRequireDefault(require("https://cdn.skypack.dev/lightgallery@2.1.2/plugins/thumbnail"));
-
-var _share = _interopRequireDefault(require("https://cdn.skypack.dev/lightgallery@2.1.2/plugins/share"));
-
-var _rotate = _interopRequireDefault(require("https://cdn.skypack.dev/lightgallery@2.1.2/plugins/rotate"));
-
-var _autoplay = _interopRequireDefault(require("https://cdn.skypack.dev/lightgallery@2.1.2/plugins/autoplay"));
-
-var _fullscreen = _interopRequireDefault(require("https://cdn.skypack.dev/lightgallery@2.1.2/plugins/fullscreen"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
-// import lightGallery from 'lightgallery';
-// import lgThumbnail from 'lightgallery/plugins/thumbnail'
-// import lgZoom from 'lightgallery/plugins/zoom'
 document.getElementById('scrollButton').addEventListener('click', function () {
   var lowerSection = document.getElementById('lowerSection');
   lowerSection.scrollIntoView({
@@ -50,26 +31,44 @@ $(document).ready(function () {
     slidesToShow: 3,
     slidesToScroll: 3
   });
-});
-$("gallery-container").justifiedGallery({
-  captions: false,
-  lastRow: "hide",
-  rowHeight: 180,
-  margins: 5
-}).on("jg.complete", function () {
-  (0, _lightgallery["default"])(document.getElementById("gallery-container"), {
-    autoplayFirstVideo: false,
-    pager: false,
-    galleryId: "nature",
-    flipHorizontal: false,
-    flipVertical: false,
-    rotateLeft: false,
-    plugins: [_zoom["default"], _thumbnail["default"], _share["default"], _rotate["default"], _fullscreen["default"], _autoplay["default"]],
-    mobileSettings: {
-      controls: false,
-      showCloseIcon: false,
-      download: false,
-      rotate: false
-    }
-  });
-});
+}); //   import lightGallery from 'lightgallery';
+// import lgThumbnail from 'lightgallery/plugins/thumbnail'
+// import lgZoom from 'lightgallery/plugins/zoom'
+//   import lightGallery from "https://cdn.skypack.dev/lightgallery@2.1.2";
+//   import lgZoom from "https://cdn.skypack.dev/lightgallery@2.1.2/plugins/zoom";
+//   import lgThumbnail from "https://cdn.skypack.dev/lightgallery@2.1.2/plugins/thumbnail";
+//   import lgShare from "https://cdn.skypack.dev/lightgallery@2.1.2/plugins/share";
+//   import lgRotate from "https://cdn.skypack.dev/lightgallery@2.1.2/plugins/rotate";
+//   import lgAutoplay from "https://cdn.skypack.dev/lightgallery@2.1.2/plugins/autoplay";
+//   import lgFullscreen from "https://cdn.skypack.dev/lightgallery@2.1.2/plugins/fullscreen";
+//   $("gallery-container")
+//     .justifiedGallery({
+//       captions: false,
+//       lastRow: "hide",
+//       rowHeight: 180,
+//       margins: 5
+//     })
+//     .on("jg.complete", function () {
+//       lightGallery(document.getElementById("gallery-container"), {
+//         autoplayFirstVideo: false,
+//         pager: false,
+//         galleryId: "nature",
+//         flipHorizontal: false,
+//         flipVertical: false,
+//         rotateLeft: false,
+//         plugins: [
+//           lgZoom,
+//           lgThumbnail,
+//           lgShare,
+//           lgRotate,
+//           lgFullscreen,
+//           lgAutoplay
+//         ],
+//         mobileSettings: {
+//           controls: false,
+//           showCloseIcon: false,
+//           download: false,
+//           rotate: false
+//         }
+//       });
+//     });
--- Project 2/script.js
+++ Project 2/script.js
@@ -1,6 +1,4 @@
-// import lightGallery from 'lightgallery';
-// import lgThumbnail from 'lightgallery/plugins/thumbnail'
-// import lgZoom from 'lightgallery/plugins/zoom'
+
 
 document.getElementById('scrollButton').addEventListener('click', function() {
   const lowerSection = document.getElementById('lowerSection');
@@ -32,48 +30,52 @@ $(document).ready(function(){
   );
   });
 
-  import lightGallery from "https://cdn.skypack.dev/lightgallery@2.1.2";
+//   import lightGallery from 'lightgallery';
+// import lgThumbnail from 'lightgallery/plugins/thumbnail'
+// import lgZoom from 'lightgallery/plugins/zoom'
 
-  import lgZoom from "https://cdn.skypack.dev/lightgallery@2.1.2/plugins/zoom";
+//   import lightGallery from "https://cdn.skypack.dev/lightgallery@2.1.2";
+
+//   import lgZoom from "https://cdn.skypack.dev/lightgallery@2.1.2/plugins/zoom";
   
-  import lgThumbnail from "https://cdn.skypack.dev/lightgallery@2.1.2/plugins/thumbnail";
+//   import lgThumbnail from "https://cdn.skypack.dev/lightgallery@2.1.2/plugins/thumbnail";
   
-  import lgShare from "https://cdn.skypack.dev/lightgallery@2.1.2/plugins/share";
+//   import lgShare from "https://cdn.skypack.dev/lightgallery@2.1.2/plugins/share";
   
-  import lgRotate from "https://cdn.skypack.dev/lightgallery@2.1.2/plugins/rotate";
+//   import lgRotate from "https://cdn.skypack.dev/lightgallery@2.1.2/plugins/rotate";
   
-  import lgAutoplay from "https://cdn.skypack.dev/lightgallery@2.1.2/plugins/autoplay";
-  import lgFullscreen from "https://cdn.skypack.dev/lightgallery@2.1.2/plugins/fullscreen";
+//   import lgAutoplay from "https://cdn.skypack.dev/lightgallery@2.1.2/plugins/autoplay";
+//   import lgFullscreen from "https://cdn.skypack.dev/lightgallery@2.1.2/plugins/fullscreen";
   
-  $("gallery-container")
-    .justifiedGallery({
-      captions: false,
-      lastRow: "hide",
-      rowHeight: 180,
-      margins: 5
-    })
-    .on("jg.complete", function () {
-      lightGallery(document.getElementById("gallery-container"), {
-        autoplayFirstVideo: false,
-        pager: false,
-        galleryId: "nature",
-        flipHorizontal: false,
-        flipVertical: false,
-        rotateLeft: false,
-        plugins: [
-          lgZoom,
-          lgThumbnail,
-          lgShare,
-          lgRotate,
-          lgFullscreen,
-          lgAutoplay
-        ],
-        mobileSettings: {
-          controls: false,
-          showCloseIcon: false,
-          download: false,
-          rotate: false
-        }
-      });
-    });
+//   $("gallery-container")
+//     .justifiedGallery({
+//       captions: false,
+//       lastRow: "hide",
+//       rowHeight: 180,
+//       margins: 5
+//     })
+//     .on("jg.complete", function () {
+//       lightGallery(document.getElementById("gallery-container"), {
+//         autoplayFirstVideo: false,
+//         pager: false,
+//         galleryId: "nature",
+//         flipHorizontal: false,
+//         flipVertical: false,
+//         rotateLeft: false,
+//         plugins: [
+//           lgZoom,
+//           lgThumbnail,
+//           lgShare,
+//           lgRotate,
+//           lgFullscreen,
+//           lgAutoplay
+//         ],
+//         mobileSettings: {
+//           controls: false,
+//           showCloseIcon: false,
+//           download: false,
+//           rotate: false
+//         }
+//       });
+//     });
   
