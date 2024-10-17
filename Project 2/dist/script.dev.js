60478b0e087db07bd934221dc41cccc75458a616
KaterynaStadnichenko <iloveua.work@gmail>
2024-10-17 20:24:10 +0300

    gallery upd

--- Project 2/dist/script.dev.js
+++ Project 2/dist/script.dev.js
@@ -31,44 +31,9 @@ $(document).ready(function () {
     slidesToShow: 3,
     slidesToScroll: 3
   });
-}); //   import lightGallery from 'lightgallery';
-// import lgThumbnail from 'lightgallery/plugins/thumbnail'
-// import lgZoom from 'lightgallery/plugins/zoom'
-//   import lightGallery from "https://cdn.skypack.dev/lightgallery@2.1.2";
-//   import lgZoom from "https://cdn.skypack.dev/lightgallery@2.1.2/plugins/zoom";
-//   import lgThumbnail from "https://cdn.skypack.dev/lightgallery@2.1.2/plugins/thumbnail";
-//   import lgShare from "https://cdn.skypack.dev/lightgallery@2.1.2/plugins/share";
-//   import lgRotate from "https://cdn.skypack.dev/lightgallery@2.1.2/plugins/rotate";
-//   import lgAutoplay from "https://cdn.skypack.dev/lightgallery@2.1.2/plugins/autoplay";
-//   import lgFullscreen from "https://cdn.skypack.dev/lightgallery@2.1.2/plugins/fullscreen";
-//   $("gallery-container")
-//     .justifiedGallery({
-//       captions: false,
-//       lastRow: "hide",
-//       rowHeight: 180,
-//       margins: 5
-//     })
-//     .on("jg.complete", function () {
-//       lightGallery(document.getElementById("gallery-container"), {
-//         autoplayFirstVideo: false,
-//         pager: false,
-//         galleryId: "nature",
-//         flipHorizontal: false,
-//         flipVertical: false,
-//         rotateLeft: false,
-//         plugins: [
-//           lgZoom,
-//           lgThumbnail,
-//           lgShare,
-//           lgRotate,
-//           lgFullscreen,
-//           lgAutoplay
-//         ],
-//         mobileSettings: {
-//           controls: false,
-//           showCloseIcon: false,
-//           download: false,
-//           rotate: false
-//         }
-//       });
-//     });
+});
+lightGallery(document.getElementById('gallery-container'), {
+  plugins: [lgZoom, lgThumbnail],
+  licenseKey: 'your_license_key',
+  speed: 500
+});
