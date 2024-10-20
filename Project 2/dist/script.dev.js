7273245845cc6e5d53473816ed6a896dc888b1a4
KaterynaStadnichenko <iloveua.work@gmail>
2024-10-20 23:06:51 +0300

    project 2 upd

--- Project 2/dist/script.dev.js
+++ Project 2/dist/script.dev.js
@@ -37,3 +37,20 @@ lightGallery(document.getElementById('gallery-container'), {
   licenseKey: 'your_license_key',
   speed: 500
 });
+var map = L.map('map').setView([37.98591543248659, 23.735404487599645], 13);
+L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png	', {
+  maxZoom: 19,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map); // const marker = L.marker([37.98591543248659, 23.735404487599645]).addTo(map);
+
+var greenIcon = L.icon({
+  iconUrl: 'https://www.svgrepo.com/show/292085/pointer-map-pointer.svg',
+  iconSize: [40, 40] // size of the icon
+  // iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+  // shadowAnchor: [4, 62],  // the same for the shadow
+  // // popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+
+});
+L.marker([37.98591543248659, 23.735404487599645], {
+  icon: greenIcon
+}).addTo(map);
